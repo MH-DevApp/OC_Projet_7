26840bec1739e833cda10b0773c77a6c3d846d01
Mehdi H <78570781+MH-DevApp@users.noreply.github.com>
2023-08-04 16:15:55 +0200

    feat (#22): Installing necessary project dependencies (#3)
    * feat (#3): Installing necessary project dependencies
    - symfony/maker-bundle (dev)
    - orm (doctrine)
    - orm-fixtures (dev)
    - symfony/serializer-pack
    - sensio/framework-extra-bundle
    - symfony/validator
    - doctrine/annotations
    - security
    - lexik/jwt-authentication-bundle
    - willdurand/hateoas-bundle
    - nelmio/api-doc-bundle
    - twig
    - asset
    
    * fix: add param DATABASE_URL in .env file (SymfonyInsight)

--- config/bundles.php
+++ config/bundles.php
@@ -2,4 +2,16 @@
 
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
+    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
+    Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
+    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
+    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
+    Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
+    JMS\SerializerBundle\JMSSerializerBundle::class => ['all' => true],
+    Bazinga\Bundle\HateoasBundle\BazingaHateoasBundle::class => ['all' => true],
+    Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
 ];
