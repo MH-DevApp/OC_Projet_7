40803c4aec7ca83259796decf992080193929661
mehdi <mehdih.devapp@gmail.com>
2023-08-06 22:10:51 +0200

    feat (#12): add groups for serialization of user entities

--- src/Entity/Customer.php
+++ src/Entity/Customer.php
@@ -6,6 +6,7 @@ use App\Repository\CustomerRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use JMS\Serializer\Annotation\Groups;
 use Symfony\Bridge\Doctrine\IdGenerator\UuidGenerator;
 use Symfony\Component\Uid\UuidV6;
 
@@ -13,9 +14,11 @@ use Symfony\Component\Uid\UuidV6;
 class Customer extends User
 {
     #[ORM\Column(length: 255)]
+    #[Groups(["getUsersByCustomer"])]
     private ?string $companyName = null;
 
     #[ORM\Column(length: 80, nullable: true)]
+    #[Groups(["getUsersByCustomer"])]
     private ?string $companySiret = null;
 
     #[ORM\OneToMany(mappedBy: 'customer', targetEntity: User::class)]
