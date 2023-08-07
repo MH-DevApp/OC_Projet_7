2f567403fac0cf9902a311fd6fe219be3caf7931
Mehdi H <78570781+MH-DevApp@users.noreply.github.com>
2023-08-07 22:20:20 +0200

    feat (#41): Writing documentation with Nelmio Api Doc for customer and product paths (#18)
    * feat (#18): add description in the documentation at the path /api/login_check
    
    * feat (#18): fix documentation api doc
    
    * feat (#18): add documentation in the product controller

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -7,6 +7,7 @@ use App\Repository\ProductRepository;
 use App\Utils\Pagination;
 use JMS\Serializer\SerializationContext;
 use JMS\Serializer\SerializerInterface;
+use Nelmio\ApiDocBundle\Annotation\Model;
 use OpenApi\Attributes as OA;
 use Psr\Cache\InvalidArgumentException;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -27,9 +28,23 @@ class ProductController extends AbstractController
      */
     #[OA\Get(
         path: '/api/products',
+        description: '<b><u>Récupération de la liste des produits</u>:</b><br><br>
+            Une pagination est mise en place. À l\'aide de query params, il est possible de saisir le numéro 
+            d\'une page et la limite de résultats par page.<br>
+            Cette liste retournera :
+            <ul>
+                <li>Nombre total de produits dans la base de données</li>
+                <li>Nombre total de produits dans la page</li>
+                <li>Nombre total de pages</li>
+                <li>La page précédente (si elle existe)</li>
+                <li>La page suivante (si elle existe)</li>
+                <li>La liste des produits avec leur détail</li>
+            </ul><br>',
+        summary: 'Récupération de la liste des produits.',
         parameters: [
             new OA\Parameter(
                 name: 'page',
+                description: 'Paramètre concernant le numéro de page (optionnel)',
                 in: 'query',
                 required: false,
                 schema: new OA\Schema(type: 'integer'),
@@ -37,11 +52,95 @@ class ProductController extends AbstractController
             ),
             new OA\Parameter(
                 name: 'limit',
+                description: 'Paramètre concernant le nombre de produits par page (optionnel)',
                 in: 'query',
                 required: false,
                 schema: new OA\Schema(type: 'integer'),
                 example: 5
             )
+        ],
+        responses: [
+            new OA\Response(
+                response: Response::HTTP_OK,
+                description: 'Retourne le nombre de produits dans la page, le nombre de produits dans la base de données, 
+                le nombre de page, la page précédente, la page suivante et la liste des produits.',
+                content: new OA\JsonContent(
+                    properties: [
+                        new OA\Property(
+                            property: 'total_items_page',
+                            type: 'integer',
+                            example: 5
+                        ),
+                        new OA\Property(
+                            property: 'total_items_collection',
+                            type: 'integer',
+                            example: 15
+                        ),
+                        new OA\Property(
+                            property: 'total_pages',
+                            type: 'integer',
+                            example: 3
+                        ),
+                        new OA\Property(
+                            property: 'previous_page',
+                            type: 'string',
+                            example: "https://localhost:3000/api/products?limit=5&page=1"
+                        ),
+                        new OA\Property(
+                            property: 'next_page',
+                            type: 'string',
+                            example: "https://localhost:3000/api/products?limit=5&page=3"
+                        ),
+                        new OA\Property(
+                            property: 'data',
+                            type: 'array',
+                            items: new OA\Items(
+                                ref: new Model(
+                                    type: Product::class
+                                ),
+                                type: 'object'
+                            )
+                        )
+                    ],
+                    type: 'object'
+                )
+            ),
+            new OA\Response(
+                response: Response::HTTP_UNAUTHORIZED,
+                description: 'Une exception est levée, l\'utilisateur doit être connecté pour accéder aux informations demandées.',
+                content: new OA\JsonContent(
+                    properties: [
+                        new OA\Property(
+                            property: 'status',
+                            type: 'int',
+                            example: 401
+                        ),
+                        new OA\Property(
+                            property: 'message',
+                            type: 'string',
+                            example: 'JWT Token not found'
+                        )
+                    ]
+                )
+            ),
+            new OA\Response(
+                response: Response::HTTP_NOT_FOUND,
+                description: 'Une exception est levée, la page n\'existe pas.',
+                content: new OA\JsonContent(
+                    properties: [
+                        new OA\Property(
+                            property: 'status',
+                            type: 'int',
+                            example: 404
+                        ),
+                        new OA\Property(
+                            property: 'message',
+                            type: 'string',
+                            example: 'Page not found.'
+                        )
+                    ]
+                )
+            ),
         ]
     )]
     #[Route('', name: 'products', methods: ['GET'])]
@@ -106,13 +205,65 @@ class ProductController extends AbstractController
      */
     #[OA\Get(
         path: '/api/products/{id}',
+        description: '<b><u>Récupération du détail d\'un produit</u>:</b><br><br>
+            Cela aura pour effet de retourner un objet JSON contenant la clé et valeur de ses propriétés.<br><br>',
+        summary: 'Récupération du détail d\'un produit à l\'aide de son identifiant.',
         parameters: [
             new OA\Parameter(
                 name: 'id',
+                description: 'Identifiant Uuid du produit que l\'on souhaite obtenir le détail des informations.',
                 in: 'path',
                 schema: new OA\Schema(type: 'string'),
                 example: '1ee33165-a193-6d0c-be5b-a7cf585beb7d'
             )
+        ],
+        responses: [
+            new OA\Response(
+                response: Response::HTTP_OK,
+                description: 'Retourne le détail du produit sous forme d\'objet JSON.',
+                content: new OA\JsonContent(
+                    ref: new Model(
+                        type: Product::class
+                    ),
+                    type: 'object'
+                )
+            ),
+            new OA\Response(
+                response: Response::HTTP_UNAUTHORIZED,
+                description: 'Une exception est levée, l\'utilisateur doit être connecté pour accéder aux informations demandées.',
+                content: new OA\JsonContent(
+                    properties: [
+                        new OA\Property(
+                            property: 'status',
+                            type: 'int',
+                            example: 401
+                        ),
+                        new OA\Property(
+                            property: 'message',
+                            type: 'string',
+                            example: 'JWT Token not found'
+                        )
+                    ]
+                )
+            ),
+            new OA\Response(
+                response: Response::HTTP_NOT_FOUND,
+                description: 'Une exception est levée, la page n\'existe pas.',
+                content: new OA\JsonContent(
+                    properties: [
+                        new OA\Property(
+                            property: 'status',
+                            type: 'int',
+                            example: 404
+                        ),
+                        new OA\Property(
+                            property: 'message',
+                            type: 'string',
+                            example: 'Page not found.'
+                        )
+                    ]
+                )
+            ),
         ]
     )]
     #[Route('/{id}', name: 'products_details', methods: ['GET'])]
